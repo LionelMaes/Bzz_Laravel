b6b64cdee660c941599549a551e80c350f9b0f83
Lionel Maes <lionel.maes@student.ehb.be>
2019-09-01 00:29:41 +0200

    create first eloquant model

---
+++ ByeWesp/app/Appointment.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Appointment extends Model
+{
+    protected $fillable =['username','date'];
+
+    public function getPosts($session){
+        if (!$session->has('posts')){
+            $this->getDummyData($session);
+        }
+        return $session->get('posts');
+    }
+}
